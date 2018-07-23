8b241d3dda0aa7768200f1021ccfd07486223ebc
Cheng Wang2 <nle5255@n-1050.office.hd>
2018-07-23 17:44:18 +0200

    nit

--- apps/helloworld/NNPACK/rename.sh
+++ apps/helloworld/NNPACK/rename.sh
@@ -1,6 +1,6 @@
 #bin/bash
 
 mv deps/cpuinfo/src/init.c deps/cpuinfo/src/cpuinfo-init.c
-mv deps/cpuinfo/src/x86/init.c deps/cpuinfo/src/cpuinfo-x86-init.c
-mv deps/cpuinfo/src/x86/linux/init.c deps/cpuinfo/src/cpuinfo-x86-linux-init.c
-mv deps/cpuinfo/src/x86/cache/init.c deps/cpuinfo/src/cpuinfo-x86-cache-init.c
+mv deps/cpuinfo/src/x86/init.c deps/cpuinfo/src/x86/cpuinfo-x86-init.c
+mv deps/cpuinfo/src/x86/linux/init.c deps/cpuinfo/src/x86/linux/cpuinfo-x86-linux-init.c
+mv deps/cpuinfo/src/x86/cache/init.c deps/cpuinfo/src/x86/cache/cpuinfo-x86-cache-init.c
